cdaa6dcde072739d569b155c23448cb872a51ea3
GlebZhidovich <gzhidovich@gmail.com>
2020-04-07 01:03:04 +0300

    feat: add drag and drop for elements

--- src/ts/field.ts
+++ src/ts/field.ts
@@ -2,6 +2,80 @@ function getRandomInt(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+let selected: HTMLElement | null = null;
+let dragSrcEl: HTMLElement | null = null;
+const cellArr: HTMLElement[] = [];
+
+function checkResult(arr: HTMLElement[]): void {
+  const result = arr.every((elem, i) => {
+    if (arr.length - 1 === i) {
+      return  elem.textContent === '';
+    }
+    return  elem.textContent === i + 1 + '';
+  });
+  console.log(result);
+}
+
+function handleDragStart(e: DragEvent): void {
+  if (this.textContent === '') {
+    return;
+  }
+  this.classList.add('drag');
+  dragSrcEl = this;
+
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+}
+
+function handleDragOver(e: DragEvent): boolean {
+  if (e.preventDefault) {
+    e.preventDefault();
+  }
+  if (this.textContent === '') {
+    e.dataTransfer.dropEffect = 'move';
+  } else {
+    e.dataTransfer.dropEffect = 'none';
+  }
+  return false;
+}
+
+function handleDragEnter(): void {
+  if (this.textContent === '') {
+    selected = this;
+    this.classList.add('over');
+  }
+}
+
+function handleDragLeave(): void {
+  if (selected) {
+    selected = null;
+    this.classList.remove('over');
+  }
+}
+
+function handleDrop(e: DragEvent): boolean {
+  if (e.stopPropagation) {
+    e.stopPropagation();
+  }
+  if (dragSrcEl != this) {
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+  return false;
+}
+
+function handleDragEnd(): void {
+  if (selected) {
+    selected.classList.remove('over');
+  }
+  this.classList.remove('drag');
+  checkResult(cellArr);
+}
+
+function addHandlers() {
+
+}
+
 export default (size: number): HTMLElement => {
   const field = document.createElement('div');
   field.classList.add('game__field');
@@ -12,11 +86,18 @@ export default (size: number): HTMLElement => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
   // @ts-ignore
   const numberArr: string[] = Array.from(numberSet);
-  console.log(numberArr);
+
   for (let i = 0; i < size**2; i++) {
       const elem = document.createElement('div');
+      cellArr.push(elem);
       elem.classList.add('game__field__cell', `m_${size}`);
-      elem.setAttribute('data-cell', numberArr[i]);
+      elem.setAttribute('draggable', 'false');
+      elem.addEventListener('dragstart', handleDragStart);
+      elem.addEventListener('dragenter', handleDragEnter);
+      elem.addEventListener('dragover', handleDragOver);
+      elem.addEventListener('dragleave', handleDragLeave);
+      elem.addEventListener('drop', handleDrop);
+      elem.addEventListener('dragend', handleDragEnd);
       if (numberArr[i] !== '0') {
         elem.append(numberArr[i]);
       }
